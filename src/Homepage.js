3fe51e9a33bcf2e9d8de79733538ad03dd85e5ea
ZachMorg <zacharymmorgan@gmail.com>
2024-03-28 16:26:13 -0400

    changed homepage to display user's name or directions to sign in

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {Link} from 'react-router-dom';
+import UserContext from './UserContext';
 
 const Homepage = function(){
+
+    const {currUser} = useContext(UserContext);
+    if(currUser){
+        return(
+            <div>
+                <h2>Welcome {currUser.firstName}!</h2>
+            </div>
+        )
+    }
+
     return(
         <div>
-            <h2>Homepage</h2>
+            <h2>Hello, sign up or login to continue.</h2>
+            <Link to={'/signup'}>Sign Up</Link>
+            <Link to={'/login'}>Login</Link>
         </div>
     )
 }
